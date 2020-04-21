11cba67bfaa1fb92606a3dfb1122b92dcd4ad500
Jarquevious <jarquevious@gmail.com>
2020-04-20 18:39:26 -0700

    changes made

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from books.models import Author, Book, Tag
 # Register your models here.
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'birth_date')
+admin.site.register(Author)
+
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'num_pages', 'date_published', 'author', 'tags')
+
+admin.site.register(Book)
+
+# class TagAdmin(admin.ModelAdmin):
+#     list_display = ('tag')
+
+admin.site.register(Tag)
+
